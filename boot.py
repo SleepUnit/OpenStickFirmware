9f9e603c9a5ba5136879bba3d6f7437ba6b9af07
Jonathan Barket <jbarket@cteh.com>
2021-08-06 17:00:07 -0500

    Initial commit. README needs work.

---
+++ boot.py
@@ -0,0 +1,67 @@
+import usb_hid
+
+OFS_DESCRIPTOR_REPORT_ID = 7
+
+ofs_descriptor = bytes((
+  0x05, 0x01,	# USAGE_PAGE (Generic Desktop)
+	0x09, 0x05,	# USAGE (Gamepad) - Very important for Switch
+	0xa1, 0x01,	# COLLECTION (Application)
+  0x85, 0xFF, # 7 [SET AT RUNTIME]
+
+	# 16 Buttons
+	0x05, 0x09,	#   USAGE_PAGE (Button)
+	0x19, 0x01,	#   USAGE_MINIMUM (Button 1)
+	0x29, 0x10,	#   USAGE_MAXIMUM (Button 16)
+	0x15, 0x00,	#   LOGICAL_MINIMUM (0)
+	0x25, 0x01,	#   LOGICAL_MAXIMUM (1)
+	0x75, 0x01,	#   REPORT_SIZE (1)
+	0x95, 0x10,	#   REPORT_COUNT (16)
+	0x55, 0x00,	#   UNIT_EXPONENT (0)
+	0x65, 0x00,	#   UNIT (None)
+	0x81, 0x02,	#   INPUT (Data,Var,Abs)
+
+	# One Hat switches (8 Positions)
+	0x05, 0x01,	#   USAGE_PAGE (Generic Desktop)
+	0x09, 0x39,	#   USAGE (Hat switch)
+	0x15, 0x00,	#   LOGICAL_MINIMUM (0)
+	0x25, 0x07,	#   LOGICAL_MAXIMUM (7)
+	0x35, 0x00,	#   PHYSICAL_MINIMUM (0)
+	0x46, 0x3B, 0x01,	      #   PHYSICAL_MAXIMUM (315)
+	0x65, 0x14,	#   UNIT (Eng Rot:Angular Pos)
+	0x75, 0x04,	#   REPORT_SIZE (4)
+	0x95, 0x01,	#   REPORT_COUNT (1)
+	0x81, 0x02,	#   INPUT (Data,Var,Abs)
+
+	0x65, 0x00,
+	0x95, 0x01,
+	0x81, 0x01,
+
+	# X, Y, and Z Axis
+	0x15, 0x00,	#   LOGICAL_MINIMUM (0)
+	0x26, 0xff, 0x00,	      #   LOGICAL_MAXIMUM (255)
+	0x75, 0x08,	#   REPORT_SIZE (8)
+	0x09, 0x01,	#   USAGE (Pointer)
+	0xA1, 0x00,	#   COLLECTION (Physical)
+	0x09, 0x30,	#     USAGE (x)
+	0x09, 0x31,	#     USAGE (y)
+	0x09, 0x32,	#     USAGE (z)
+	0x09, 0x35,	#     USAGE (rz)
+	0x95, 0x04,	#     REPORT_COUNT (4)
+	0x81, 0x02,	#     INPUT (Data,Var,Abs)
+	0xc0,	#   END_COLLECTION
+	0xc0	# END_COLLECTION
+))
+
+ofs = usb_hid.Device(
+    report_descriptor = ofs_descriptor,
+    usage_page = 0x1,
+    usage = 0x5,
+    in_report_length = 7,
+    out_report_length = 1,
+    report_id_index = OFS_DESCRIPTOR_REPORT_ID,
+)
+
+devices = [ofs]
+usb_hid.enable(devices)
+
+print("OpenFightStick HID Device Activated.")
---
+++ ofs.py
@@ -0,0 +1,117 @@
+# Copyright (c) 2021 Jonathan Barket
+# Copyright (c) 2018 Dan Halbert for Adafruit Industries 
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from adafruit_hid.keyboard import find_device
+import usb_hid
+import struct
+import time
+
+class OpenFightStick:
+    def __init__(self, devices):
+        self._device = find_device(devices, usage_page=0x1, usage=0x05)
+
+        # report[0] buttons 1-8 (LSB is button 1)
+        # report[1] buttons 9-16
+        # report[2] hat 0-8
+        # report[3] joystick 0 x: -127 to 127
+        # report[4] joystick 0 y: -127 to 127
+        # report[5] joystick 1 x: -127 to 127
+        # report[6] joystick 1 y: -127 to 127
+        self._report = bytearray(7)
+
+        # Remember the last report as well, so we can avoid sending
+        # duplicate reports.
+        self._last_report = bytearray(7)
+
+        # Store settings separately before putting into report. Saves code
+        # especially for buttons.
+        self._buttons_state = 0
+        self._hat_position = 8
+        self._joy_x = 0
+        self._joy_y = 0
+        self._joy_z = 0
+        self._joy_r_z = 0
+
+        # Send an initial report to test if HID device is ready.
+        # If not, wait a bit and try once more.
+        try:
+            self.reset_all()
+        except OSError:
+            time.sleep(1)
+            self.reset_all()
+
+    def press_buttons(self, *buttons):
+        for button in buttons:
+            self._buttons_state |= 1 << button - 1
+        self._send()
+
+    def release_buttons(self, *buttons):
+        for button in buttons:
+            self._buttons_state &= ~(1 << button - 1)
+        self._send()
+
+    def move_hat(self, direction):
+      """ DPad is interpreted as a hat. Values are 0-7 where N is 0, and the
+      directions move clockwise. 8 is centered. """
+      self._hat_position = direction
+      self._send()
+
+    def move_joysticks(self, x=None, y=None, z=None, r_z=None):
+        if x is not None:
+            self._joy_x = x
+        if y is not None:
+            self._joy_y = y
+        if z is not None:
+            self._joy_z = z
+        if r_z is not None:
+            self._joy_r_z = r_z
+        self._send()
+
+    def reset_all(self):
+        """Return the fightstick to a neutral state"""
+        self._buttons_state = 0
+        self._hat_position = 8
+        self._joy_x = 0
+        self._joy_y = 0
+        self._joy_z = 0
+        self._joy_r_z = 0
+        self._send(always=True)
+
+    def _send(self, always=False):
+        """Send a report with all the existing settings.
+        If ``always`` is ``False`` (the default), send only if there have been changes.
+        """
+        struct.pack_into(
+            "<HBbbbb",
+            self._report,
+            0,
+            self._buttons_state,
+            self._hat_position,
+            self._joy_x,
+            self._joy_y,
+            self._joy_z,
+            self._joy_r_z,
+        )
+
+        if always or self._last_report != self._report:
+            self._device.send_report(self._report)
+            self._last_report[:] = self._report
+
